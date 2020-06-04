d3dac0926b60bceb75505909ba8b09e1762aad9c
scurry222 <scurry222@gmail.com>
2020-06-03 20:26:54 -0700

    fixed PORT for deployment

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__, template_folder='templates', static_url_path='/static/')
+
+PORT = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=PORT)
 
 @app.route("/")
 def home():
