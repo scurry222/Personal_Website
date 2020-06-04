33f8ad8dd8417eca652dd1016bae86450a3e6336
scurry222 <scurry222@gmail.com>
2020-06-03 20:42:15 -0700

    remove bad port

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ from flask import Flask, render_template
 from jinja2 import StrictUndefined
 import os
 
-app = Flask(__name__, template_folder='templates', static_url_path='/static/')
+app = Flask(__name__)
 
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "ABCDEF")
 app.jinja_env.undefined = StrictUndefined
 
-PORT = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=PORT)
-
 @app.route("/")
 def home():
-    return render_template('index.html')
+    """Home page"""
+
+    return render_template("index.html")
+
 
 @app.route("/error")
 def error():
